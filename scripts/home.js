47fdbf6e2ba443e03db54114869691eaf96c0647
Katie Ta <katie_ta@brown.edu>
2017-04-11 21:06:32 -0400

    updated js and forms

---
+++ scripts/home.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $('.messageButton').on("click", sendMessage);
+
+  function sendMessage() {
+    console.log("message button clicked");
+  }
+    // then tell server what to search for and which filters!!
+  
+});
